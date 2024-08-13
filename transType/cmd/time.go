09e07a0c1b951e9d5bfb9e3216a4f618fbf6ba89
steveLin43 <tai45612360@gmail.com>
2024-08-13 13:45:23 +0800

    timeTool

---
+++ transType/cmd/time.go
@@ -0,0 +1,54 @@
+var calculateTime string
+var duration string
+// 使用標準函式庫的 RFC3339 標準格式
+t := time.Now().Format(time.RFC3339)
+
+var timeCmd = &cobra.Command{
+	Use: "time",
+	Short: "時間格式處理",
+	Long: "時間格式處理",
+	Run: func(cmd *cobra.Command, args []string) {},
+}
+
+var nowTimeCmd = &cobra.Command{
+	Use: "now",
+	Short: "取得目前時間",
+	Long: "取得目前時間",
+	Run: func(cmd *cobra.Command, args []string) {
+		nowTime := timer.GetNowTime()
+		log.Printf("輸出結果: %s, %d", nowTime.Format("2006-01-0215:04:05"), nowTime.Unix())
+	},
+}
+
+var calculateTimeCmd = &cobra.Command{
+	Use: "calc",
+	Short: "計算所需時間",
+	Long: "計算所需時間",
+	Run: func(cmd *cobra.Command, args []string) {
+		var currentTimer time.Time
+		var layout = "2006-01-0215:04:05"
+		if calculateTime == "" {
+			currentTimer = timer.GetNowTime()
+		} else {
+			var err error
+			space := strings.Count(calculateTime, " ")  //strings.Contains?
+			if space == 0 {
+				layout = "2006-01-02"
+			}
+			if space == 1 {
+				layout = "2006-01-0215:04"
+			}
+			currentTimer, err = time.Parse(layout, calculateTime)
+			if err != nil {
+				t, _ := strconv.Atoi(calculateTime)
+				currentTimer = time.Unix(int64(t), 0)
+			}
+		}
+		t, err := timer.GetCalculateTime(currentTimer,duration)
+		if err != nil {
+			log.Fatalf("timer.GetCalculateTime err: %v", err)
+		}
+
+		log.Printf("輸出結果: %s, %d", t.Format(layout), t.Unix())
+	},
+}
---
+++ transType/internal/timer/time.go
@@ -0,0 +1,12 @@
+func GetNowTime() time.Time {
+	location, _ := time.LoadLocation("Asia/Shanghai")  // 考量時區
+	return time.Now().In(location)
+}
+
+func GetCalculateTime(currentTimer time.Time, d string) (time.Time, error) {
+	duration, err := time.ParseDuration(d)  // 解析出持續時間
+	if err != nil {
+		return time.Time{}, err
+	}
+	return currentTimer.Add(duration), nil // 目前時間 + 持續時間
+}
